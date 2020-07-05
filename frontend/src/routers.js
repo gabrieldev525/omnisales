22c22b14e1dad9c95623d9202c4998258e676205
garciamendes <garciaprog0101@gmail.com>
2020-07-05 20:46:04 -0300

    adjustment

--- frontend/src/routers.js
+++ frontend/src/routers.js
@@ -4,12 +4,19 @@ import React from 'react'
 // project imports
 import MainPage from './containers/main'
 import ProductDetail from './containers/product/product-detail'
+import CustomDashboard from './containers/dashboard'
+import Leads from './containers/leads/config-lead'
 
 // third imports
 import {
   FiAirplay,
   FiTrendingUp
 } from 'react-icons/fi'
+import {
+  MdDashboard,
+  MdRecordVoiceOver
+} from 'react-icons/md'
+
 
 export const routes = [
   {
@@ -27,6 +34,14 @@ export const routes = [
     menu: true,
     name: 'Detalhe do produto',
     icon: <FiTrendingUp />
+  },
+  {
+    path: '/product/test/leads',
+    component: Leads,
+    exact: false,
+    menu: true,
+    name: 'Detalhe do produto',
+    icon: <MdRecordVoiceOver />
   },
 ]
 
