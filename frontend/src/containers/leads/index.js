6c0722b8c7279202f817dde0c8a9c765ca9291e7
jotasenati <jsenadiniz@gmail.com>
2020-07-05 19:17:47 -0300

    Format Document if 2 spaces

--- frontend/src/containers/leads/index.js
+++ frontend/src/containers/leads/index.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import { Tabs, Tab, Panel } from '@bumaga/tabs'
-import '../../static/css/leads.scss' 
+import '../../static/css/leads.scss'
 import TableLead from './lead';
 
 export default () => (
-    <Tabs className="tabs">
-        <div>
-            <Tab><button className="tab1">Lista de leads</button></Tab>
-            <Tab><button className="tab2">Configurações</button></Tab>
-        </div>
+  <Tabs className="tabs">
+    <div>
+      <Tab><button className="tab1">Lista de leads</button></Tab>
+      <Tab><button className="tab2">Configurações</button></Tab>
+    </div>
 
-        <Panel><p><TableLead /></p></Panel>
-        <Panel><p>Esse queima</p></Panel>
-    </Tabs>
+    <Panel><p><TableLead /></p></Panel>
+    <Panel><p>Esse queima</p></Panel>
+  </Tabs>
 );
+ 
--- frontend/src/containers/leads/lead.js
+++ frontend/src/containers/leads/lead.js
@@ -19,40 +19,40 @@ const leads = [
     nome: 'Jonatas Diniz',
     email: 'jsenadiniz@gmail.com',
     celular: '(11) 9 89305764',
-    dataRegistro: '20/06/2020'    
+    dataRegistro: '20/06/2020'
   },
   {
     nome: 'Matheus Faria',
     email: 'M.faria@gmail.com',
     celular: '(83) 9 30584758',
-    dataRegistro: '20/06/2020'    
+    dataRegistro: '20/06/2020'
   },
   {
     nome: 'Gabriel Victor',
     email: 'g.victor@hotmail.com',
     celular: '(83) 9 58478568',
-    dataRegistro: '20/06/2020'    
+    dataRegistro: '20/06/2020'
   },
   {
     nome: 'Vamberto',
     email: 'vamberto@gmail.com',
     celular: '(85) 9 83568475',
-    dataRegistro: '20/06/2020'    
+    dataRegistro: '20/06/2020'
   },
   {
     nome: 'Victor',
     email: 'victor@gmail.com',
     celular: '(81) 9 85658745',
-    dataRegistro: '20/06/2020'    
+    dataRegistro: '20/06/2020'
   },
-  
+
 ]
 
 
 const tableLead = () => {
   return (
     <div className='table-list-content'>
-      <div className='container-header'>        
+      <div className='container-header'>
       </div>
       <table>
         <thead>
@@ -60,22 +60,22 @@ const tableLead = () => {
             <th>nome</th>
             <th>Email</th>
             <th>celular</th>
-            <th>Data de Entrada</th>            
+            <th>Data de Entrada</th>
           </tr>
         </thead>
         <tbody>
-          { leads.map((item, index) => {
-                       
+          {leads.map((item, index) => {
 
-              return (
-                <tr key={index}>
-                  <td id="nome">{item.nome}</td>
-                  <td id="Email">{item.email}</td>
-                  <td id="celular">{item.celular}</td>
-                  <td id="dataRegistro">{item.dataRegistro}</td>
-                </tr>
-              )
-            })
+
+            return (
+              <tr key={index}>
+                <td id="nome">{item.nome}</td>
+                <td id="Email">{item.email}</td>
+                <td id="celular">{item.celular}</td>
+                <td id="dataRegistro">{item.dataRegistro}</td>
+              </tr>
+            )
+          })
           }
         </tbody>
       </table>
